663dc6d9884be755014bcd028aaa6a61946852ea
Marcjohn <125807622+xterbenn@users.noreply.github.com>
2025-12-08 22:06:52 +0800

    Update schoolform10.php

--- src/UI-Admin/contents/schoolform10.php
+++ src/UI-Admin/contents/schoolform10.php
@@ -23,7 +23,7 @@ if ($student_id) {
     $stmt->execute([$student_id]);
     $student = $stmt->fetch(PDO::FETCH_ASSOC);
 }
-$saveDir = 'C:/xampp/htdocs/sta.MariaSystem/sf10_files';
+$saveDir = 'C:/xampp/htdocs/sta.MariaSystem-main/sf10_files';
 if (!is_dir($saveDir)) mkdir($saveDir, 0777, true);
 
 $grades = $sections = $school_years = $advisers = [];
@@ -191,7 +191,7 @@ $stmt->execute(array_values($personal_data));
 
    
     try {
-        $template_path = 'C:/xampp/htdocs/sta.MariaSystem/src/UI-Admin/contents/sf10/sf10.xlsx';
+        $template_path = 'C:/xampp/htdocs/sta.MariaSystem-main/src/UI-Admin/contents/sf10/sf10.xlsx';
         $spreadsheet = IOFactory::load($template_path);
         $sheet = $spreadsheet->getSheet(0);
         $sheet_back = $spreadsheet->getSheet(1);
@@ -323,7 +323,7 @@ foreach ($certifications as $cert) {
         $drawing = new Drawing();
         $drawing->setName('DepEd Logo');
         $drawing->setDescription('DepEd Logo');
-        $drawing->setPath('C:/xampp/htdocs/sta.MariaSystem/assets/image/deped.png');
+        $drawing->setPath('C:/xampp/htdocs/sta.MariaSystem-main/assets/image/deped.png');
         $drawing->setCoordinates('A1');
         $drawing->setWidth(80);
         $drawing->setHeight(80);
